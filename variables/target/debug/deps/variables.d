e89e4398f972b6eaf2581c2838e236ef693695b1
j4xtr1x <github.sleek021@passmail.net>
2024-05-28 21:24:53 -0300

    Until Data Types section

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\livew\Documentos\Projetos\Learning-Rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\livew\Documentos\Projetos\Learning-Rust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\livew\Documentos\Projetos\Learning-Rust\variables\target\debug\variables.exe: C:\Users\livew\Documentos\Projetos\Learning-Rust\variables\src\main.rs
